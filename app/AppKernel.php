277964a85ebc1203af248dce83741b40bbf2d111
Stefan <stadolf@gmail.com>
2017-06-12 23:07:57 +0200

    updated composer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,10 +48,10 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-	$envFile = __DIR__.'/../.env';
-	if (file_exists($envFile)) {
-		(new Dotenv())->load($envFile);
-	}
+        $envFile = __DIR__.'/../.env';
+        if (file_exists($envFile)) {
+            (new Dotenv())->load($envFile);
+        }
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
