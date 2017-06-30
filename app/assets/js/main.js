dfea272a8943b2a471926fc95172b8407a614f90
Stefan Adolf <stadolf@gmail.com>
2017-07-01 00:44:57 +0200

    Encore (#9)
    * exchanged gulp/browserify with webpack/Symfony encore (using fork because of silly comma error)
    upgraded to npm5
    
    * reverted templates :)

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
+require('../css/main.scss');
+
 import $ from 'jquery';
+global.$ = global.jQuery = $;
+
 import 'tether';
 import 'bootstrap';
+
 import _ from 'lodash';
 
 $(() => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
